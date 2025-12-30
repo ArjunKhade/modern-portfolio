d62a1a09065f4db47aa9f601956010d6ca22a448
DESKTOP-OSI02RS\khade <khadearjun@gmail.com>
2025-12-30 22:05:38 +0530

    corrected spacing between the words

--- src/app/personal-information/personal-information.component.ts
+++ src/app/personal-information/personal-information.component.ts
@@ -17,9 +17,9 @@ export class PersonalInformationComponent {
   ];
 
   aboutMe: string[] = [
-    'Hi, I am a software Engineer with 3+ year of experience in software industry.',
-    'Worked as Sottware Developer in Aloha Technology for various technologies(Angular, ReactJS, Java, SpringBoot, .NetCore Web API)',
+    'Hi, I am a software Engineer with 3+ years of experience in software industry.',
+    'Worked as Software Developer in Aloha Technology for various technologies like Angular, ReactJS, Java, SpringBoot, .NetCore Web API',
     'Delivered all products within deadlines. Always eager to learn new technologies',
-    'Currently, working as Full Stack Software Developer At Aloha Technology Pune (Maharashtra)',
+    'Currently, working as Full Stack Software Developer At Aloha Technology Pune, Maharashtra',
   ];
 }
